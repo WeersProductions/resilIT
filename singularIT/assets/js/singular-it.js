100332da28710b0b9abfc82259cca9754b091f48
Lorian Coltof <loriancoltof@gmail.com>
2018-11-15 17:17:06 +0100

    Fix javascript shit on IE

--- singularIT/assets/js/singular-it.js
+++ singularIT/assets/js/singular-it.js
@@ -10,7 +10,9 @@ particlesJS.load('particles-js-light-2', 'assets/particlejs-light.json', functio
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+        const tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
     return a;
 }
--- singularIT/public/js/app.js
+++ singularIT/public/js/app.js
@@ -10,51 +10,32 @@ particlesJS.load('particles-js-light-2', 'assets/particlejs-light.json', functio
 function shuffle(a) {
     for (let i = a.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [a[i], a[j]] = [a[j], a[i]];
+        const tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
     }
     return a;
 }
 
 var SILVER_LOGO_DURATION = 5000;
 
-var bronze_logos = Array();
 var silver_logos = Array();
-
-bronze_logos.push("/images/snic_sponsor_logo/Logo_Belsimpel.png");
-bronze_logos.push("/images/snic_sponsor_logo/Logo_ASML.png");
-
-silver_logos.push("/images/snic_sponsor_logo/Logo_KPMG.png");
-silver_logos.push("/images/snic_sponsor_logo/Logo_DNB.png");
 silver_logos.push("/images/snic_sponsor_logo/Logo_Topicus.png");
 silver_logos.push("/images/snic_sponsor_logo/Logo_Procam.png");
 silver_logos.push("/images/snic_sponsor_logo/Logo_DSW.png");
 
-
-bronze_logos = shuffle(bronze_logos);
 silver_logos = shuffle(silver_logos);
-var bronze_logos_iter = 0;
 var silver_logos_iter = 0;
 
 function changeSilverSponsorImage() {
   // var random_logo = silver_logos[Math.floor(Math.random() * silver_logos.length)];
-  var logo = bronze_logos[bronze_logos_iter];
-  var logo_2 = silver_logos[silver_logos_iter];
-  $("#bronze_logos").fadeOut(250, function() {
-    $("#bronze_logos").attr("src", logo);
-    $("#bronze_logos").fadeIn(250);
-  });
-
+  var logo = silver_logos[silver_logos_iter];
   $("#silver_logos").fadeOut(250, function() {
-    $("#silver_logos").attr("src", logo_2);
+    $("#silver_logos").attr("src", logo);
     $("#silver_logos").fadeIn(250);
   });
 
-  bronze_logos_iter += 1;
   silver_logos_iter += 1;
-  if (bronze_logos_iter >= bronze_logos.length) {
-    bronze_logos_iter = 0;
-  }
-
   if (silver_logos_iter >= silver_logos.length) {
     silver_logos_iter = 0;
   }
