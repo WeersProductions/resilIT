9193a13ab8eb61a002a59fcf713326cd2cb305e5
Floris <weersproductions@gmail.com>
2019-10-08 18:13:24 +0200

    Fix profile

--- singularIT/routes/index.js
+++ singularIT/routes/index.js
@@ -139,23 +139,23 @@ async function canEnrollForSession(sessionId) {
 /**
  * Queries the database to get all the visitor counts for non plenary sessions.
  */
-async function getVisitorCounts(){
-  // Query database to check how many people are going to each session
-  promises = [];
-  for (var sessionidx = Object.keys(speakerinfo.speakerids).length - 1; sessionidx >= 0; sessionidx--) {
-    var session = Object.keys(speakerinfo.speakerids)[sessionidx];
-    // Filter out the plenary sessions
-    if ( speakerinfo.speakerids[session] instanceof Array) {
-      for (var speakeridx = speakerinfo.speakerids[session].length - 1; speakeridx >= 0; speakeridx--) {
-        var speaker = speakerinfo.speakerids[session][speakeridx];
-        promises.push(countEnrolls(session, speaker));
-      }
-    }
-  }
+// async function getVisitorCounts(){
+//   // Query database to check how many people are going to each session
+//   promises = [];
+//   for (var sessionidx = Object.keys(speakerinfo.speakerids).length - 1; sessionidx >= 0; sessionidx--) {
+//     var session = Object.keys(speakerinfo.speakerids)[sessionidx];
+//     // Filter out the plenary sessions
+//     if ( speakerinfo.speakerids[session] instanceof Array) {
+//       for (var speakeridx = speakerinfo.speakerids[session].length - 1; speakeridx >= 0; speakeridx--) {
+//         var speaker = speakerinfo.speakerids[session][speakeridx];
+//         promises.push(countEnrolls(session, speaker));
+//       }
+//     }
+//   }
 
-  // Gather all the data and make a dicht with
-  return Promise.all(promises);
-}
+//   // Gather all the data and make a dicht with
+//   return Promise.all(promises);
+// }
 
 router.get('/', function (req, res) {
   res.render('index', { title: '', ticketSaleStarts:config.ticketSaleStarts });
@@ -199,7 +199,7 @@ router.get('/profile', auth, async function (req, res) {
 
   // Don't try to unescape here, it's not stored in user.
   // Do it in the template
-  var visitorCounts = await getVisitorCounts();
+  // var visitorCounts = await getVisitorCounts();
 
   res.render('profile', {
     userHasBus: config.verenigingen[user.vereniging].bus,
@@ -207,7 +207,6 @@ router.get('/profile', auth, async function (req, res) {
     speakerids: speakerinfo.speakerids,
     speakers: speakerinfo.speakers,
     matchingterms:config.matchingterms,
-    visitorCounts: visitorCounts,
     spTimeSlot: spTimeSlot,
     allSpTimeSlots: allSpTimeSlots,
     freeSpTimeSlots: freeSpTimeSlots,
@@ -274,12 +273,16 @@ router.post('/api/talks/enroll/:id', auth, async function(req, res) {
         user: user,
         talk: req.params.id
       });
-      newTalkEnrollment.save(function(err) {
-        if(err) {
-          res.json({success: false});
-        } else {
-          res.json({success: true});
-        }
+      newTalkEnrollment.save(
+        // function(err) {
+        // if(err) {
+        //   res.json({success: false});
+        // } else {
+        //   res.json({success: true});
+        // }
+      // }
+      ).then(function(document) {
+        console.log(document);
       });
     }
   });
@@ -461,128 +464,13 @@ router.get('/api/favorite/:id', auth, async function (req, res) {
   });
 })
 
-/**
- * This function is used to determine if there is still room for someone to
- * enroll and takes in to account if someone is already enrolled.
- * TODO: possibly combine with countEnrolls?
- */
-// async function canEnrollForSession(sessionslot, sessionid, useremail){
-//   if (Date.now() >= new Date(config.provideTrackPreferencesEnd).getTime()) {
-//     return false;
-//   }
-
-//   if(typeof sessionid == "undefined" || sessionid == "" || sessionid == null){
-//     return true;
-//   }
-
-//   var session = speakerinfo.speakers.filter(function(speaker){
-//     return speaker.id == sessionid;
-//   });
-
-//   // session not found
-//   if (session.length != 1) {
-//     return false;
-//   }
-
-//   session = session[0];
-
-//   // Check if there is a limit and if so, if it has been reached
-//   if (session.limit) {
-//     var query = {};
-//     query[sessionslot] = sessionid;
-//     var result;
-
-//     await User
-//       .find(query)
-//       .where('email')
-//       .ne(useremail)
-//       .count()
-//       .then(function(res){
-//         result = res;
-//       });
-//     return result < session.limit;
-//   }
-
-//   return true;
-// }
-
 router.post('/profile', auth, async function (req, res) {
   req.sanitize('vegetarian').toBoolean();
   req.sanitize('bus').toBoolean();
   req.sanitize('allowBadgeScanning').toBoolean();
 
-  if(typeof req.body.session1 === 'undefined'){
-    req.body.session1 = '';
-  }
-
-  if(typeof req.body.session2 === 'undefined'){
-    req.body.session2 = '';
-  }
-
-  if(typeof req.body.session3 == 'undefined'){
-    req.body.session3 = '';
-  }
-
-
-  if(req.body.session1 !== "" && req.body.session1 !== null && !speakerinfo.speakerids.session1.includes(req.body.session1)){
-    req.flash('error', "session1 went wrong!");
-    return res.redirect('/profile');
-  }
-  if(req.body.session2 !== "" && req.body.session2 !== null && !speakerinfo.speakerids.session2.includes(req.body.session2)){
-    req.flash('error', "session2 went wrong!");
-    return res.redirect('/profile');
-  }
-  if(req.body.session3 !== "" && req.body.session3 !== null && !speakerinfo.speakerids.session3.includes(req.body.session3)){
-    req.flash('error', "session3 went wrong!");
-    return res.redirect('/profile');
-  }
-
   User.findOne({email:req.session.passport.user}).exec( async function (err, user) {
   if (!err){
-/*******************************************************************************
- * There is some form of race condition possible. the check if the session is
- * full can be done after someone else has been checked but before he has been
- * enrolled.
- *
- * Best would be to do a conditional update, however, Mongo does not support
- * this feature in mongo 3.4.
- *
- * For now this is not as big as a problem because one person extra is not
- * that big of a problem. However, watch carefully if people actively abuse
- * this
- ******************************************************************************/
-
-      var canEnrollSession1 = await canEnrollForSession("session1", req.body.session1,
-        req.session.passport.user);
-      var canEnrollSession2 = await canEnrollForSession("session2", req.body.session2,
-        req.session.passport.user);
-      var canEnrollSession3 = await canEnrollForSession("session3", req.body.session3,
-        req.session.passport.user);
-
-      var tracksClosed = Date.now() >= new Date(config.provideTrackPreferencesEnd).getTime()
-
-      // naar functie zetten en samenvoegen
-      if( canEnrollSession1 ){
-        user.session1 = req.body.session1;
-      } else if(!tracksClosed) {
-        req.flash('error', "It is not possible to sign up for the talk you chose for the first session. It's possibly full.");
-        err = true;
-      }
-
-      if( canEnrollSession2 ){
-        user.session2 = req.body.session2;
-      } else if(!tracksClosed) {
-        req.flash('error', "It is not possible to sign up for the talk you chose for the second session. It's possibly full.");
-        err = true;
-      }
-
-      if (canEnrollSession3){
-        user.session3 = req.body.session3;
-      } else if(!tracksClosed) {
-        req.flash('error', "It is not possible to sign up for the talk you chose for the third session. It's possibly full.");
-        err = true;
-      }
-
       user.vegetarian   = req.body.vegetarian ? true : false;
       user.bus          = req.body.bus ? true : false;
       user.specialNeeds = req.body.specialNeeds;
